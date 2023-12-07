27940b9ccfe0ddfb70b7a181fa73fea330db2b41
romyjkk <98404201+romyjkk@users.noreply.github.com>
2023-12-07 23:43:06 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,15 +9,12 @@ buttonPistool.addEventListener("click", playPistool);
 
 function playVergrootglas() {
   video.src = "videos/wow.mp4";
-  console.log("run mooi");
 }
 
 function playBoos() {
   video.src = "videos/boos.mp4";
-  console.log("run matig");
 }
 
 function playPistool() {
   video.src = "videos/gun.mp4";
-  console.log("sup slecht");
 }
